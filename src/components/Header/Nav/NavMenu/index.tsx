ef67a4866f045ce5dcf887c53b7e26274af3a1b7
hyseh <edgyhyseh@gmail.com>
2024-05-07 09:20:05 +0200

    Fixed menus staying open when routing to a new route

--- src/components/Header/Nav/NavMenu/index.tsx
+++ src/components/Header/Nav/NavMenu/index.tsx
@@ -9,34 +9,30 @@ type NavMenuProps = {
 
 export default function NavMenu({ setIsOpen }: NavMenuProps) {
   return (
-    <div
-      className="fixed left-0 top-0 flex h-full w-full justify-end bg-black/30 p-4"
-      onClick={() => setIsOpen(false)}
-    >
+    <div className="fixed left-0 top-0 z-[1000] flex h-full w-full justify-end bg-black/30 p-4">
       <nav className="flex h-full w-full max-w-80 flex-col gap-2 rounded-lg bg-background px-4 py-5 shadow">
         <div className="flex justify-between">
-          <a href="/" aria-label="Holidaze" className="inline-flex">
-            <span className="flex items-center gap-1 py-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="h-6 w-6"
-              >
-                <path
-                  fill="#5E5EF3"
-                  d="M0 12a4 4 0 1 1 8 0v8a4 4 0 0 1-8 0v-8Zm16-8a4 4 0 0 1 8 0v16a4 4 0 0 1-8 0V4Zm-8 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"
-                />
-                <path
-                  fill="#5E5EF3"
-                  fillRule="evenodd"
-                  d="M4 0a4 4 0 1 0 0 8h3.407A.593.593 0 0 0 8 7.407V4a4 4 0 0 0-4-4Zm0 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="font-bold text-brand">Holidaze</span>
-            </span>
-          </a>
+          <div className="flex items-center gap-1 py-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 w-6"
+            >
+              <path
+                fill="#5E5EF3"
+                d="M0 12a4 4 0 1 1 8 0v8a4 4 0 0 1-8 0v-8Zm16-8a4 4 0 0 1 8 0v16a4 4 0 0 1-8 0V4Zm-8 8a4 4 0 1 1 8 0 4 4 0 0 1-8 0Z"
+              />
+              <path
+                fill="#5E5EF3"
+                fillRule="evenodd"
+                d="M4 0a4 4 0 1 0 0 8h3.407A.593.593 0 0 0 8 7.407V4a4 4 0 0 0-4-4Zm0 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <span className="font-bold text-brand">Holidaze</span>
+          </div>
+
           <TertiaryButton size="default" onClick={() => setIsOpen(false)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -55,6 +51,7 @@ export default function NavMenu({ setIsOpen }: NavMenuProps) {
             <span>Close</span>
           </TertiaryButton>
         </div>
+
         <div className="flex h-full flex-col justify-between">
           <div className="flex flex-col">
             {routes.map((route, index) => {
@@ -62,10 +59,11 @@ export default function NavMenu({ setIsOpen }: NavMenuProps) {
                 <NavLink
                   key={index}
                   to={route.href}
+                  onClick={() => setIsOpen(false)}
                   className={({ isActive }) =>
                     isActive
-                      ? 'inline-flex rounded-full py-2 font-bold text-text'
-                      : 'inline-flex rounded-full py-2 text-text hover:text-text-muted'
+                      ? 'inline-flex rounded-md py-2 font-bold text-text lg:rounded-full'
+                      : 'inline-flex rounded-md py-2 text-text hover:text-text-muted lg:rounded-full'
                   }
                 >
                   {route.title}
@@ -73,6 +71,7 @@ export default function NavMenu({ setIsOpen }: NavMenuProps) {
               )
             })}
           </div>
+
           <PrimaryButton size="full">Sign in</PrimaryButton>
         </div>
       </nav>
--- src/components/Header/Profile/ProfileMenu/index.tsx
+++ src/components/Header/Profile/ProfileMenu/index.tsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom'
 import { routes } from '../data'
 
-export default function ProfileMenu() {
+type ProfileMenuProps = {
+  setIsOpen: (value: boolean) => void
+}
+
+export default function ProfileMenu({ setIsOpen }: ProfileMenuProps) {
   return (
     <div className="absolute right-0 mt-2 flex min-w-60 flex-col whitespace-nowrap rounded-lg bg-background py-2 shadow">
       {routes.map((route, index) => {
@@ -9,6 +13,7 @@ export default function ProfileMenu() {
           <Link
             key={index}
             to={route.href}
+            onClick={() => setIsOpen(false)}
             className="inline-flex w-full px-5 py-3 hover:bg-background-alt"
           >
             {route.title}
