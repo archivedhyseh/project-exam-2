671b076a89f27e60fd5a9d76bf635eff3bb23c67
hyseh <edgyhyseh@gmail.com>
2024-05-08 03:29:50 +0200

    Fixed names being too long in VenueCard component

--- src/components/Cards/VenueCard/VenueDetails/index.tsx
+++ src/components/Cards/VenueCard/VenueDetails/index.tsx
@@ -7,7 +7,7 @@ export default function VenueDetails({ name, price }: VenueDetailsProps) {
   return (
     <div>
       <div>
-        <span className="line-clamp-1 font-bold text-text">
+        <span className="line-clamp-1 text-ellipsis break-all font-bold text-text">
           {name.trim().length > 1 ? name : 'No name for venue'}
         </span>
       </div>
