5939a17664e72be5b026e4f1313c805dda028208
hyseh <edgyhyseh@gmail.com>
2024-05-18 22:02:05 +0200

    Moved fetch outside of route

--- src/routes/Listing/index.tsx
+++ src/routes/Listing/index.tsx
@@ -3,31 +3,39 @@ import { useParams } from 'react-router-dom'
 import { Errors, Venue } from '../../api/types'
 import VenueListing from '../../components/Cards/VenueListing'
 
+type fetchListingProps = {
+  id: string | undefined
+}
+
+const fetchListing = async ({ id }: fetchListingProps) => {
+  let url =
+    'https://v2.api.noroff.dev/holidaze/venues' +
+    `/${id}` +
+    '?_owner=true' +
+    '&_bookings=true'
+
+  const res = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+
+  const { data, errors }: { data: Venue; errors: Errors[] } = await res.json()
+
+  if (errors) {
+    throw new Error(errors[0].message)
+  }
+
+  return data
+}
+
 export default function Listing() {
   const { id } = useParams()
 
   const { data, error, isSuccess, isError } = useQuery({
-    queryKey: ['listing'],
-    queryFn: async () => {
-      const res = await fetch(
-        `https://v2.api.noroff.dev/holidaze/venues/${id}?_owner=true&_bookings=true`,
-        {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      )
-
-      const { data, errors }: { data: Venue; errors: Errors[] } =
-        await res.json()
-
-      if (errors) {
-        throw new Error(errors[0].message)
-      }
-
-      return data
-    },
+    queryKey: ['listing', id],
+    queryFn: () => fetchListing({ id }),
   })
 
   return (
