e71e82d547cabf95faa8156f28f985cce631f10f
hyseh <edgyhyseh@gmail.com>
2024-05-24 21:50:55 +0200

    Updated height of main content

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -8,7 +8,9 @@ export default function Layout({ children }: LayoutProps) {
   return (
     <>
       <Header />
-      <main className="mb-5 flex-1 lg:mb-4">{children}</main>
+      <main className="mb-5 min-h-[calc(100dvh-82px-20px)] lg:mb-4 lg:min-h-[calc(100dvh-82px-16px)]">
+        {children}
+      </main>
       <Footer />
     </>
   )
