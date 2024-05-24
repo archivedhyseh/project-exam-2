a97b9d5ccf9b2c3b10fb0a54e482b52736de8db3
hyseh <edgyhyseh@gmail.com>
2024-05-25 00:59:50 +0200

    Updated formatting functions

--- src/utility/format.ts
+++ src/utility/format.ts
@@ -1,6 +1,9 @@
 export function formatName(name: string) {
   if (name) {
     if (name.trim().length > 1) {
+      if (name.toLowerCase() === 'string') {
+        return 'Venue without a name'
+      }
     }
     return name.charAt(0).toUpperCase() + name.slice(1)
   }
@@ -22,18 +25,6 @@ export function formatCity(city: string | null) {
 export function formatCountry(country: string | null) {
   if (country) {
     if (country.trim().length > 1) {
-      if (country.toLowerCase() === 'norge' || 'noreg') {
-        return 'Norway'
-      }
-      if (country.toLowerCase() === 'united arab emirates' || 'uae') {
-        return 'UAE'
-      }
-      if (country.toLowerCase() === 'united kingdom' || 'uk') {
-        return 'UK'
-      }
-      if (country.toLowerCase() === 'united states' || 'usa') {
-        return 'USA'
-      }
       if (country.toLowerCase() === 'string') {
         return 'Unknown'
       }
