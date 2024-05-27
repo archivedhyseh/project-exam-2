1c8e269025277808ead0b67e731a9d795ade985b
hyseh <edgyhyseh@gmail.com>
2024-05-27 21:55:30 +0200

    Removed ManageCard component

--- src/routes/Dashboard/ManageVenues/index.tsx
+++ src/routes/Dashboard/ManageVenues/index.tsx
@@ -1,24 +1,33 @@
 import { Venue } from '../../../api/types'
-import ManageCard from '../../../components/Cards/ManageCard'
+import VenueCard from '../../../components/Cards/VenueCard'
 
 type ManageVenuesProps = {
   venues: Venue[]
 }
 
 export default function ManageVenues({ venues }: ManageVenuesProps) {
-  venues.sort((a, b) => {
-    const createdA = Date.parse(a.created)
-    const createdB = Date.parse(b.created)
+  if (venues) {
+    venues.sort((a, b) => {
+      const createdA = Date.parse(a.created)
+      const createdB = Date.parse(b.created)
 
-    return createdA - createdB
-  })
+      return createdA - createdB
+    })
+  }
 
   return (
     <>
       {venues.length > 0 ? (
         <div className="grid auto-rows-fr gap-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          {venues.map((venue) => (
-            <ManageCard key={venue.id} venue={venue} />
+          {venues.map(({ id, name, media, price, location }) => (
+            <VenueCard
+              key={id}
+              id={id}
+              name={name}
+              media={media}
+              price={price}
+              location={location}
+            />
           ))}
         </div>
       ) : (
