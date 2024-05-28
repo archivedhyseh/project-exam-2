0a62a751c6ff4244890a7adf0f536d84fa924fc0
hyseh <edgyhyseh@gmail.com>
2024-05-28 02:28:05 +0200

    Fixed styling issue in Tabs component

--- src/components/Tabs/TabsList/index.tsx
+++ src/components/Tabs/TabsList/index.tsx
@@ -6,7 +6,7 @@ type TabsListProps = {
 
 export default function TabsList({ children }: TabsListProps) {
   return (
-    <div className="overflow-hidden overflow-x-auto py-1">
+    <div className="overflow-hidden overflow-x-auto p-1">
       <div className="flex">{children}</div>
 
       <hr className="border-black-alt" />
