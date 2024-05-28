1a2db6ab173a162f7e3112e7e583b56d80bf43c2
hyseh <edgyhyseh@gmail.com>
2024-05-28 22:30:50 +0200

    Updated Support route

--- src/routes/Support/index.tsx
+++ src/routes/Support/index.tsx
@@ -1,3 +1,29 @@
 export default function Support() {
-  return <div>Support</div>
+  return (
+    <div className="mx-auto max-w-screen-2xl px-4 py-5 lg:py-4">
+      <div className="sm:p-10 lg:p-10">
+        <div className="mx-auto flex w-full max-w-3xl flex-col gap-8">
+          <div className="grid gap-2 sm:gap-0">
+            <h1 className="text-balance text-center text-3xl font-semibold text-text">
+              We are here to help
+            </h1>
+
+            <p className="text-balance text-center text-xl text-text-muted">
+              Get answers to your questions or read our guides and articles
+            </p>
+          </div>
+
+          <p className="text-pretty text-text">
+            We apologise for the inconvenience, but the support page is
+            currently under development. If you need immediate support, please
+            reach out to our support email below. We appreciate your patience
+            and understanding as we work to improve your experience. Thank you
+            for your understanding.
+          </p>
+
+          <span className="font-semibold text-text">support@holidaze.com</span>
+        </div>
+      </div>
+    </div>
+  )
 }
