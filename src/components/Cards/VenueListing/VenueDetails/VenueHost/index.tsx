0eda6c41cc103a63f389f6a3dd354a218c4853a3
hyseh <edgyhyseh@gmail.com>
2024-05-28 23:38:30 +0200

    Fixed VenueHost issue in VenueListing component

--- src/components/Cards/VenueListing/VenueDetails/VenueHost/index.tsx
+++ src/components/Cards/VenueListing/VenueDetails/VenueHost/index.tsx
@@ -7,14 +7,20 @@ type VenueHostProps = {
 export default function VenueHost({ owner }: VenueHostProps) {
   return (
     <div className="flex max-w-screen-md items-center gap-2">
-      <div className="h-12 w-12">
-        <img
-          src={owner.avatar.url}
-          alt={owner.avatar.alt || `Image of ${owner.name}'s avatar`}
-          className="aspect-square h-full w-full overflow-hidden rounded-full object-cover"
-        />
-      </div>
-      <span className="font-bold text-text">Hosted by {owner.name}</span>
+      {owner ? (
+        <>
+          <div className="h-12 w-12">
+            <img
+              src={owner.avatar.url}
+              alt={owner.avatar.alt || `Image of ${owner.name}'s avatar`}
+              className="aspect-square h-full w-full overflow-hidden rounded-full object-cover"
+            />
+          </div>
+          <span className="font-bold text-text">Hosted by {owner.name}</span>
+        </>
+      ) : (
+        <span className="text-text">Unable to load the host of the venue.</span>
+      )}
     </div>
   )
 }
