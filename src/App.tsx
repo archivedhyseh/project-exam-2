2c5a6920e2edcf89f76f424ab5f329e0f41c2dcc
hyseh <edgyhyseh@gmail.com>
2024-05-28 23:08:05 +0200

    Fixed scroll restoration issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, ScrollRestoration } from 'react-router-dom'
 import Layout from './layout'
 
 export default function App() {
   return (
     <Layout>
+      <ScrollRestoration
+        getKey={(location) => {
+          return location.pathname
+        }}
+      />
       <Outlet />
     </Layout>
   )
